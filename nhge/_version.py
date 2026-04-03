0a1e70a0c2db0347b1a6dfabbc8a1040183050ca
mwala400 <hekimamwala1@gmail.com>
2026-04-04 00:42:15 +0300

    fix: force clean versioning

--- nhge/_version.py
+++ nhge/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.0.post5'
-__version_tuple__ = version_tuple = (0, 1, 0, 'post5')
+__version__ = version = '0.1.5.post0'
+__version_tuple__ = version_tuple = (0, 1, 5, 'post0')
 
-__commit_id__ = commit_id = 'g15b16b403'
+__commit_id__ = commit_id = 'g314a3f63a'
